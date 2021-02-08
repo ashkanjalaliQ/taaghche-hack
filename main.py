f89becab0c6c36d6488a749aee53de36e21c61c5
Ashkan Jalali <55811147+ashkanjalaliQ@users.noreply.github.com>
2021-02-08 09:09:08 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from time import sleep
+import os
 
 class Taaghche:
     def __init__(self, driver):
         self.driver = driver
         self.login_page = 'https://taaghche.com/'
-        self.page_number = 1
+        self.total_pages = 0
+        self.current_page_number = 0
 
     def login(self, username, password):
         login_bottun_class = 'profileDropdown_login__1mKoW'
@@ -44,36 +45,50 @@ class Taaghche:
         sleep(10)
         self.driver.switch_to.window(driver.window_handles[1])
 
-    def input_checker(self):
+    def __input_checker(self):
         input_value = input()
         if input_value == 'start':
             return input_value
 
-    def select_book(self):
+    def select_book(self, id):
         self.__open_my_lib()
-        #self.driver.execute_script(f'window.open("{self.book_reader_link}","_blank");')
-        #return self.book_reader_link
-        if self.input_checker() == 'start':
-            self.page_book()
-    def page_book(self):
-        self.driver.maximize_window()
-        t = 920
-        while t != 0:
-            self.__screen_shot(self.driver)
-            self.control_reader_page(left=True)
-            t += 1
+        
+        if self.__input_checker() == 'start':
+            self.driver.maximize_window()
+            self.total_pages = int(self.__get_total_pages())
+            print(self.total_pages)
+            self.__page_book(id)
 
+    def __page_book(self, id):
+        self.page_number = int(self.__get_current_page_number())
+        while self.page_number <= self.total_pages:
+            self.__screen_shot(id)
+            self.__control_reader_page(next=True)
+            self.__situation()
+            self.page_number += 1
 
-    def control_reader_page(self, left=False, right=False):
-        if left:
+    def __get_total_pages(self):
+        totalpages = self.driver.find_element_by_id('totalPages')
+        totalpages = totalpages.text
+        return totalpages
+
+    def __situation(self):
+        print(f'[*]Page {self.page_number} of {self.total_pages}')
+
+    def __get_current_page_number(self):
+        current_page = self.driver.find_element_by_id('pageNo')
+        current_page = current_page.text
+        return current_page
+
+    def __control_reader_page(self, next=False, previous=False):
+        if next:
             self.driver.find_element_by_id('___nextPage').click()
 
-        elif right:
+        elif previous:
             self.driver.find_element_by_id('___prevPage').click()
 
-    def __screen_shot(self, driver):
-        driver.save_screenshot(f'{self.page_number}-book.jpg')
-        self.page_number += 1
+    def __screen_shot(self, id):
+        self.driver.save_screenshot(f'page{self.page_number}-{id}.jpg')
 
 
 driver_address = 'chromedriver.exe'
@@ -88,4 +103,4 @@ info = {
 
 taaghche.login(username=info['username'], password=info['password'])
 
-taaghche.select_book()
+taaghche.select_book(id=39639)
